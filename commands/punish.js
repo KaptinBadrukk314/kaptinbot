a212dd9b8eb93772034a7c7b42f2ed30095a8907
benhamlin314 <31233276+benhamlin314@users.noreply.github.com>
2022-02-05 17:59:07 -0800

    Update view printout of all punishments in database

--- commands/punish.js
+++ commands/punish.js
@@ -74,9 +74,9 @@ module.exports = {
          await interaction.reply({content:'Your punishment has been added. For it to become active, other users must vote on your punishment to activate it.', ephemeral: true})
       } else if(interaction.options.getSubcommand() === 'view'){
          const temp = await Punishment.findAll();
-         let punishments = "Name---Description---Vote Count---Active\n";
+         let punishments = "Name---Description---Vote Count---Actived---ModActivated\n";
          temp.forEach((item) => {
-            const temp2 = `${item.name}---${item.description}---${item.voteCount}---${item.activeFlg}\n`
+            const temp2 = `${item.name}---${item.description}---${item.voteCount}---${item.activeFlg}---${item.modActivate}\n`
             punishments = punishments.concat(temp2);
          });
          let embed = new MessageEmbed()
